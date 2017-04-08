81e60e67bf934fbbad8178e9de1e0a7baf400afd
Nishant Srivastava <nisrulz@gmail.com>
2017-04-08 13:39:10 -0700

    added matrix code

---
+++ java/Matrix_Code/matrix.java
@@ -0,0 +1,32 @@
+class matrix {
+  public static void main(String Args[]) {
+    int i = 0, j = 0;
+    for (i = 0; i < 200; i++) {
+
+      System.out.println("E R B V C F G T U 7 6 8 4 6 5 9 5 7 2 H J G K H L J K D N");
+
+      for (j = 0; j < 99999999; j++)
+        ;
+
+      System.out.println("T U Y B B V 6 7 8 9 6 9 7 0 J H V F U F 7 8 9 6 7 9 8 6 9");
+
+      for (j = 0; j < 99999999; j++)
+        ;
+
+      System.out.println("H J U Y T G V 8 9 8 7 6 3 6 5 7 8 K J G H I U I 8 9 9 8 6");
+
+      for (j = 0; j < 99999999; j++)
+        ;
+
+      System.out.println("L K J K J F C G X X 9 8 7 7 8 9 6 9 J K L H K G 8 9 0 J H");
+
+      for (j = 0; j < 99999999; j++)
+        ;
+
+      System.out.println("8 7 9 6 9 Y T U T I G B K G V F 8 7 9 7 6 9 7 9 8 7 H G J");
+
+      for (j = 0; j < 99999999; j++)
+        ;
+    }
+  }
+}
